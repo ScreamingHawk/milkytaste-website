d9a6f38613a69e900adfb46e214e261a79a65845
Michael Standen <screaminghawk@gmail.com>
2021-07-29 12:05:46 +1200

    Replace landing pgae

---
+++ src/components/Logo.js
@@ -0,0 +1,18 @@
+import styled from "styled-components"
+
+const Dot = styled.div`
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	background: #AEC6CF;
+	border-radius: 50%;
+	font-size: 5em;
+	height: 2em;
+	width: 2em;
+`
+
+const Logo = () => (
+	<Dot>🥛</Dot>
+)
+
+export default Logo
