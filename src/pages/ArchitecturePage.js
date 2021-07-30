a43a4678d4808d74e0bd6c23eb20d0245701be15
Michael Standen <screaminghawk@gmail.com>
2021-07-30 23:13:05 +1200

    Add architecture image

--- src/pages/ArchitecturePage.js
+++ src/pages/ArchitecturePage.js
@@ -1,6 +1,9 @@
 import { Link } from 'react-router-dom';
 import Header from '../components/Header';
 import PageContent from '../components/PageContent';
+import { CircleImg } from '../components/Image';
+
+import architecture from '../images/architecture.png';
 
 const ArchitecturePage = () => {
   return (
@@ -8,10 +11,11 @@ const ArchitecturePage = () => {
       <Header />
       <PageContent>
         <h1>Software Architecture</h1>
+        <CircleImg src={architecture} />
         <p>
           MilkyTaste has corporate experience designing and implementing complex systems for industry leaders.
           <br />
-          These services can be available for you to help guide the development of your project.
+          These services can be available for you to help guide the development of your project, no matter the size or budget.
         </p>
         <p>
           Developing on the blockchain can be very different from traditional web and application development.
