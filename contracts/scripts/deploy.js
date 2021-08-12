3e058af178877892d7686a16028a88bd13701082
Michael Standen <screaminghawk@gmail.com>
2021-08-12 15:11:17 +1200

    Contract deploy writes output to frontend

--- contracts/scripts/deploy.js
+++ contracts/scripts/deploy.js
@@ -25,6 +25,33 @@ async function main() {
 	)
 
 	console.log("MilkToken deployed to:", milkToken.address);
+
+	saveToFrontend(milkToken.address, network.name)
+}
+
+function saveToFrontend(address, networkName) {
+	const fs = require('fs')
+	const contractsDir = `${__dirname}/../../src/contracts/${networkName.toLowerCase()}`
+
+	if (!fs.existsSync(contractsDir)) {
+		fs.mkdirSync(contractsDir)
+	}
+
+	// Create address file
+	const fileData = {
+		"MilkToken": address,
+	}
+	fs.writeFileSync(
+		`${contractsDir}/contract-address.json`,
+		JSON.stringify(fileData, undefined, 2)
+	)
+
+	const Artifact = artifacts.readArtifactSync("MilkToken")
+
+	fs.writeFileSync(
+		`${contractsDir}/MilkToken.json`,
+		JSON.stringify(Artifact, null, 2)
+	)
 }
 
 main()
